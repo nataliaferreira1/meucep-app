a9a92c5d12abd8615d34eaf91a18866ad5be407c
NATALIA FERREIRA SOUSA <591182348@alunos.etec.itaqua>
2019-07-30 22:08:03 -0300

    criação de logica e isolamento de maétodo para salvar endereços, e chamada API viacep

--- src/app/add-endereco/add-endereco.page.ts
+++ src/app/add-endereco/add-endereco.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import Endereco from '../domain/Endereco';
 
 @Component({
   selector: 'app-add-endereco',
@@ -7,14 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEnderecoPage implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private alert:AlertController, private nav:NavController) { }
 
   ngOnInit() {
   }
-buscar(cep){
+  buscar(cep) {
 
-const cepString = cep.el.value
-const cepValido = cepString.replace('1', 'olá')
-console.log(cepValido)
-}
+    const cepString = cep.el.value
+    if (cepString === '' || cepString.length !== 8 || !cepString.match(/^\d+$/g )) {
+      console.log("CEP Inválido");
+    }else{
+      console.log("CEP Válido");
+      let retorno = fetch('https://viacep.com.br/ws/' + cepString + '/json')
+      console.log("Enviando requisição...")
+      retorno.then(dados =>{
+        return dados.json()
+      }).then(endereco => {
+        if(endereco.erro){
+          console.error('CEP Inexistente')
+        }else{
+          this.alert.create({
+            header:'Seu endereço está correto?',
+            subHeader: `${endereco.logradouro}, ${endereco.bairro}, ${endereco.localidade} - ${endereco.uf.toUpperCase()}`,
+            buttons: [{
+              text: 'Não'
+            }, {
+              text: 'Sim',
+              handler: ()=> {
+                this.alert.dismiss()
+                this.alert.create({
+                  header:"Qual seu número?",
+                  inputs:[{
+                    name:"numero",
+                    type:"number"
+                  }],
+                  buttons:[{
+                    text: 'Cancelar'
+                  }, {
+                    text: 'Salvar',
+                    handler:(dados)=>{
+                      let tempEnd = new Endereco()
+                      tempEnd.bairro = endereco.bairro
+                      tempEnd.cidade = endereco.localidade
+                      tempEnd.estado = endereco.uf
+                      tempEnd.numero = dados.numero
+                      tempEnd.rua = endereco.logradouro
+                      tempEnd.cep = endereco.cep
+                      tempEnd.salvar()
+                      this.nav.back()
+                    }
+                  }]
+                }).then(alert => {
+                  alert.present()
+                })
+              }
+            }]
+          }).then(alert => {
+            alert.present()
+          })
+        }
+      })
+    }
+  }
 }
--- src/app/domain/endereco.ts
+++ src/app/domain/endereco.ts
@@ -5,6 +5,10 @@ class Endereco {
     bairro:String;
     cidade:String;
     estado:String;
+
+    salvar(){
+        localStorage.setItem((Math.random() * 100).toString(), JSON.stringify(this))
+    }
 }
 
 export default Endereco
